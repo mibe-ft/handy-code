1ea49b4a7e0a7c65ffcccce8ded16ade51a54338
Maka <maka.ibe@ft.com>
2021-04-20 12:30:44 +0100

    Add query to get names of all columns

--- jira-tickets/BI-1180/adhoc_queries.sql
+++ jira-tickets/BI-1180/adhoc_queries.sql
@@ -1,3 +1,9 @@
 --
 SELECT  MIN(current_max_stage_timestamp), MAX(current_max_stage_timestamp)
 FROM `ft-bi-team.BI_layer_tables.funnels_b2b_nnb_add` LIMIT 1000
+
+-- get column names
+
+SELECT column_name
+FROM ft-bi-team.BI_layer_tables.INFORMATION_SCHEMA.COLUMNS
+WHERE table_name = 'funnels_b2b_nnb_add'
