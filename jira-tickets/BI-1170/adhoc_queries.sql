7d5dfa936aa0f55ac1725028e759fae7d9de3e71
Maka <maka.ibe@ft.com>
2021-04-23 12:47:04 +0100

    Add query from Maria

--- jira-tickets/BI-1170/adhoc_queries.sql
+++ jira-tickets/BI-1170/adhoc_queries.sql
@@ -327,3 +327,18 @@ FROM
 WHERE
 	ftuserguid IN ('9d0df5a3-a799-4760-ba33-b3f5ae78650e');
 
+-- query from Maria initial to join tables
+select distinct fu.ft_user_id as user_guid
+, userstatus_dtm
+, arrangementeventdate_dkey
+, case when is_print then 'Print' else 'Digital' end print_digital
+, daa.b2c_marketing_region
+, daa.to_arrangementtype_name
+, daa.to_arrangementlength_id
+from dwabstraction.fact_userstatus fu
+join  dwabstraction.dn_arrangementevent_all daa
+    on fu.user_dkey = daa.user_dkey
+    and fu.userstatus_date_dkey >= daa.arrangementeventdate_dkey
+WHERE  daa.to_datasource_dkey = 2 -- Zuora
+    and fu.is_b2c is true
+
