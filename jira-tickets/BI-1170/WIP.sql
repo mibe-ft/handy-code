5e66c59b8b46223f16622ad0f34a0c227186def2
Maka <maka.ibe@ft.com>
2021-04-26 19:22:12 +0100

    Filter on one day

--- jira-tickets/BI-1170/WIP.sql
+++ jira-tickets/BI-1170/WIP.sql
@@ -1,12 +1,3 @@
-/*
- * Questions:
- * - write truncate in airflow or write append daily?
- * - do we need to filter on print and digital only? Bundle is also available in arrangement_all table
- * - how far back do we need to go with this table?
- * - do we need flag for active subscribers only? people who haven't initiated cancellation request
- * - what do we mean by 'current offer' - is this the price they are paying or the %discount compared to the current rrp??
- * */
-
 WITH user_facts AS (
 -- get b2c subs status for each day
 	SELECT
@@ -20,8 +11,8 @@ WITH user_facts AS (
 	WHERE
 --		    user_dkey = 389
 	--	AND user_dkey IN (260, 389, 10799463, 2874, 10807934, 7521, 19114, 20806, 26752, 30489, 32698, 10813871) -- randomly picked b2c users
---		AND userstatus_date_dkey = 20210423
-		 is_b2c = True
+	 	userstatus_date_dkey = 20210423
+		AND is_b2c = True
 		AND user_dkey IN (SELECT user_dkey FROM dwabstraction.dn_arrangement_all WHERE to_datasource_dkey = 2)-- make sure zuora only
 )
 , b2c_subscriptions AS (
@@ -57,8 +48,8 @@ WITH user_facts AS (
 -- output for final table
 	SELECT
 		  uf.ft_user_id
-		, uf.user_dkey
-		, uf.userstatus_date_dkey
+		, uf.user_dkey 
+		, uf.userstatus_date_dkey 
 		, uf.userstatus_dtm
 		, bsu.arrangementevent_dtm
 		, bsu.to_termstart_dtm
@@ -74,7 +65,7 @@ WITH user_facts AS (
 		, bsu.to_currency_code
 		, bsu.to_currency_name
 		, bsu.b2c_marketing_region
-
+	
 	FROM user_facts uf
 	LEFT JOIN b2c_subscriptions bsu ON uf.user_dkey = bsu.user_dkey_b
 		  AND (uf.userstatus_date_dkey >= bsu.to_termstartdate_dkey)
@@ -85,19 +76,19 @@ WITH user_facts AS (
 )
 
 -- select columns for table out and format field names
-SELECT
+SELECT 
 	  ft_user_id 									AS ft_user_guid
 	, userstatus_dtm 								AS "date"
 	, to_arrangementproduct_type 					AS print_or_digital
 	, to_priceinctax 								AS current_price
 	, COALESCE(to_offer_rrp, 9999)					AS rrp_price
 	, 100-COALESCE(to_offer_percent_rrp, 9999)		AS current_discount
---	, AS current_offer
+--	, AS current_offer 
 	, b2c_marketing_region 							AS region
 	, to_arrangementproduct_name 					AS product_name
 	, to_arrangementlength_id 						AS product_term
 FROM final_tbl
-WHERE
+WHERE 
 	to_arrangementproduct_type IN ('Print', 'Digital')
 -- AND user_dkey = 389
 -- AND userstatus_date_dkey IN (20171203, 20181203, 20191203, 20201203)
