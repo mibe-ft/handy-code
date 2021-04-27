75c47570c512b44034d4912cdd1f4c114bf5e8c0
Maka <maka.ibe@ft.com>
2021-04-27 14:26:34 +0100

    First draft of rewrite

--- jira-tickets/BI-1180/WIP.sql
+++ jira-tickets/BI-1180/WIP.sql
@@ -1,8 +1,9 @@
 SELECT 
-	  sf_log.lead_id__c  as lead_id
+	  sf_log.lead_id__c  as id
 	, sf_log."name" 
 	, sf_log.createddate 
-	, sf_leads.leadsource
+	, sf_log.lead_id__c AS lead_id
+	, sf_leads.leadsource AS original_lead_source
 --	, adjusted_lead_source -- this is derived from a case statement lines 3156 - 3237
 	, sf_leads.industry_sector AS lead_industry_sector -- d
 	, sf_leads.segment_id AS salesforce_lead_segment_id --d
@@ -21,30 +22,31 @@ SELECT
 	, sf_opps.ownerid AS oppo_owner_id --c
 	, (sf_users.firstname::text || ' '::character varying::text) || sf_users.lastname::text AS oppo_owner_name --ee
 	, sf_users.team AS oppo_owner_team --ee
---	, vo.gbpamount oppo_amount_gbp --vo
 	, CASE WHEN sf_contracts.currencyisocode::text = 'GBP'::character varying::text THEN sf_contracts.amount
 		    ELSE sf_contracts.amount / x.fxrate
-			END AS oppo_amount_gbp
+			END AS oppo_amount_gbp --	, vo.gbpamount oppo_amount_gbp --vo
 	, sf_opps.closed_lost_reason AS oppo_closed_lost_reason --c
 	, sf_opps.closedate AS oppo_closed_date --c
 	, sf_opps."type" AS oppo_product_name --c
---	, contract_amount_gbp --vc
---	, contract_start_date --h
---	, contract_total_core_readers --h
---	, contract_total_licenced_readers --h
---	, contract_licence_type --h
---	, contract_licence_name --h
---	, contract_licence_solution --h
---	, contract_type --h
---	, contract_owner_id --h
---	, contract_owner_name --f
---	, contract_owner_team --f
---	, client_type --h
---	, contractnumber --h
---	, sf_contract_number --h
---	, spoor_id --d line 6551
---	, visit_segment_id --v.campaign_id 6587
---	, visit_marketing_campaign_name --cs.marketing_campaign__r_name
+	, CASE WHEN sf_contracts.currencyisocode::text = 'GBP'::character varying::text THEN sf_contracts.amount
+		    ELSE sf_contracts.amount / x.fxrate
+			END AS contract_amount_gbp --	, contract_amount_gbp --vc
+	, sf_contracts.startdate AS contract_start_date --h
+	, sf_contracts.total_core_readers AS contract_total_core_readers --h
+	, sf_contracts.total_licenced_readers AS contract_total_licenced_readers --h
+	, sf_contracts.licencetype AS contract_licence_type --h
+	, sf_contracts.licencee_name AS contract_licence_name --h
+	, sf_contracts.licence_solution AS contract_licence_solution --h
+	, sf_contracts."type" AS contract_type --h
+	, sf_contracts.ownerid AS contract_owner_id --h
+	, (sf_users.firstname::text || ' '::character varying::text) || sf_users.lastname::text AS contract_owner_name --f
+	, sf_users.team AS contract_owner_team --f
+	, sf_contracts.client_type  --h
+	, sf_contracts.contractnumber --h
+	, sf_contracts.contractnumber_c AS sf_contract_number --h
+	, sf_leads.spoor_id --d line 6551
+	, v.campaign_id AS visit_segment_id --v.campaign_id 6587
+	, sf_cpseg.marketing_campaign__r_name::text AS visit_marketing_campaign_name --cs.marketing_campaign__r_name
 	
 FROM ftsfdb.view_sfdc_stage_log sf_log 
 LEFT JOIN ftsfdb.view_sfdc_leads sf_leads ON sf_log.lead_id__c = sf_leads.id
@@ -55,4 +57,9 @@ LEFT JOIN ftsfdb.view_sfdc_opportunities sf_opps ON sf_opps.id::text = sf_log.op
 LEFT JOIN ftsfdb.view_sfdc_contracts sf_contracts ON sf_opps.accountid::text = sf_contracts.accountid::text
 LEFT JOIN dwabstraction.dn_currencyexchangerate x ON x.fromcurrency_code = sf_contracts.currencyisocode::character(3) 
 												  AND x.fxyear = sf_contracts.startdate 
-WHERE sf_log.lead_id__c = '00Q4G000019UYfAUAW' 
+	LEFT JOIN biteam.conversion_visit c ON
+		sf_leads.spoor_id::text = c.device_spoor_id::text
+		AND c.system_action::text = 'b2b-confirmed'::character varying::text
+	LEFT JOIN ftspoordb.visits v ON
+		c.conversion_visit_id = v.visit_id
+WHERE sf_log.lead_id__c = '00Q4G00001BPPN8UAP'--'00Q4G000019UYfAUAW' 
