b4a700f86f2d5bdac286477e8080ed785bfc4484
Maka <maka.ibe@ft.com>
2021-04-27 11:28:31 +0100

    Add aliases to colums

--- jira-tickets/BI-1180/WIP.sql
+++ jira-tickets/BI-1180/WIP.sql
@@ -3,45 +3,45 @@ SELECT
 	, vslog."name" 
 	, vslog.createddate 
 	, vsl.leadsource
---	, adjusted_lead_source
---	, lead_industry_sector
---	, salesforce_lead_segment_id
---	, salesforce_lead_segment_id_name
---	, lead_spoor_id
---	, createdbyid
---	, lead_owner_id
---	, lead_owner_name
---	, lead_owner_team
---	, lead_region
---	, lead_subregion
---	, lead_country
---	, lead_gclid
---	, lead_cpc
---	, oppo_id
---	, oppo_owner_id
---	, oppo_owner_name
---	, oppo_owner_team
---	, oppo_amount_gbp
---	, oppo_closed_lost_reason
---	, oppo_closed_date
---	, oppo_product_name
---	, contract_amount_gbp
---	, contract_start_date
---	, contract_total_core_readers
---	, contract_total_licenced_readers
---	, contract_licence_type
---	, contract_licence_name
---	, contract_licence_solution
---	, contract_type
---	, contract_owner_id
---	, contract_owner_name
---	, contract_owner_team
---	, client_type
---	, contractnumber
---	, sf_contract_number
---	, spoor_id
---	, visit_segment_id
---	, visit_marketing_campaign_name
+--	, adjusted_lead_source -- this is derived from a case statement lines 3156 - 3237
+--	, lead_industry_sector -- d
+--	, salesforce_lead_segment_id --d
+--	, salesforce_lead_segment_id_name --de
+--	, lead_spoor_id --d
+--	, createdbyid -- d
+--	, lead_owner_id --d
+--	, lead_owner_name --e 
+--	, lead_owner_team --e
+--	, lead_region --r
+--	, lead_subregion --r
+--	, lead_country --d
+--	, lead_gclid --d
+--	, lead_cpc --d
+--	, oppo_id --s 
+--	, oppo_owner_id --c
+--	, oppo_owner_name --ee
+--	, oppo_owner_team --ee
+--	, oppo_amount_gbp --vo
+--	, oppo_closed_lost_reason --c
+--	, oppo_closed_date --c
+--	, oppo_product_name --c
+--	, contract_amount_gbp --vc
+--	, contract_start_date --h
+--	, contract_total_core_readers --h
+--	, contract_total_licenced_readers --h
+--	, contract_licence_type --h
+--	, contract_licence_name --h
+--	, contract_licence_solution --h
+--	, contract_type --h
+--	, contract_owner_id --h
+--	, contract_owner_name --f
+--	, contract_owner_team --f
+--	, client_type --h
+--	, contractnumber --h
+--	, sf_contract_number --h
+--	, spoor_id --d line 6551
+--	, visit_segment_id --v.campaign_id 6587
+--	, visit_marketing_campaign_name --cs.marketing_campaign__r_name
 	
 FROM ftsfdb.view_sfdc_stage_log vslog 
 JOIN ftsfdb.view_sfdc_leads vsl ON vslog.lead_id__c = vsl.id
