3b8c3357a3b35bc24898f7bb42273e1b8d7bf168
Maka <maka.ibe@ft.com>
2021-04-28 09:47:30 +0100

    Add query from Jono

---
+++ jira-tickets/BI-1170/jono_query.sql
@@ -0,0 +1,10 @@
+-- get latest status per arrangement per day
+-- person may have multiple changes in one day, we want to filter for the latest change 
+
+SELECT
+    DATE(ae.arrangementevent_dtm) AS day,
+    ae.arrangement_id_dd,
+    ae.arrangementevent_dtm,
+    ROW_NUMBER() OVER(PARTITION BY ae.arrangement_id_dd, DATE(ae.arrangementevent_dtm) ORDER BY ae.event_seq_no DESC, ae.arrangementevent_dtm DESC) AS row_num
+FROM
+    `ft-data.dwpresentation.vw_nopii_dn_arrangementevent_all` ae
