a7408499de3074deb18f33ff7b66766b0390bff4
Maka <maka.ibe@ft.com>
2021-04-28 13:38:26 +0100

    Add validation check

--- jira-tickets/BI-1170/data_validation.sql
+++ jira-tickets/BI-1170/data_validation.sql
@@ -18,6 +18,14 @@ ORDER BY arrangementevent_dtm
 
 ----
 -- one line per ft-user per arrangement id per day
+SELECT ft_user_guid
+, arrangement_id_dd
+, date_
+, COUNT(DISTINCT date_) count_of_date
+FROM dataset
+GROUP BY 1,2,3
+HAVING count_of_date > 1
+-- limit 1 -- if 1st row > 1 then fail check
 
 -- 001702c0-afb6-4c64-9779-94cd106d4884 - has two entries for 23-04-2021
 -- checking for null values in specific 'important columns'
