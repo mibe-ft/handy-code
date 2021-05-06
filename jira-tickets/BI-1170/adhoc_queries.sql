e3e19dbee38cb55d75966d824a6566279c72ec99
Maka Ibe <maka.ibe@ft.com>
2021-05-06 11:03:19 +0100

    Add starter code for step up matrix

--- jira-tickets/BI-1170/adhoc_queries.sql
+++ jira-tickets/BI-1170/adhoc_queries.sql
@@ -264,4 +264,25 @@ ORDER BY arrangementevent_dtm
 
 -- check arrangement event types
 select DISTINCT arrangementevent_name , arrangementevent_dkey 
-from dwabstraction.dn_arrangementevent_all daa 
+from dwabstraction.dn_arrangementevent_all daa
+
+-- creating the step up matrix
+
+DROP TABLE IF EXISTS #stepup_matrix;
+CREATE TABLE #stepup_matrix (
+currency VARCHAR,
+lower_band FLOAT,
+higher_band FLOAT,
+new_price FLOAT,
+percent_discount VARCHAR,
+code INT,
+offer_id VARCHAR,
+subs_term VARCHAR,
+subs_product VARCHAR
+);
+
+INSERT INTO #stepup_matrix VALUES
+('GBP',0.01,146.30,154,'-50%',1,'bb776c53-abdd-280d-4279-cd9aeb0257ff','annual','standard'),
+('GBP',146.31,196.65,207,'-33%',2,'a9582121-87c2-09a7-0cc0-4caf594985d5','annual','standard')
+;
+select * from #stepup_matrix
