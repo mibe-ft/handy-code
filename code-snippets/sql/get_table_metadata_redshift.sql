d2feaa8b1dd1f4be5a0a8559468b9b19bdead1f7
Maka Ibe <maka.ibe@ft.com>
2021-06-15 17:16:37 +0100

    Place query within CTE

--- code-snippets/sql/get_table_metadata_redshift.sql
+++ code-snippets/sql/get_table_metadata_redshift.sql
@@ -1,3 +1,6 @@
+with a as (
+select 'vw_step_up_b2c_zuora' as tbl_name
+)
 select t.table_schema as schema_name,
        t.table_name as view_name,
        c.column_name,
@@ -13,6 +16,6 @@ join information_schema.columns c
               and t.table_name = c.table_name
 where table_type = 'VIEW'
       and t.table_schema  not in ('information_schema', 'pg_catalog')
-      and t.table_name in ('vw_step_up_b2c_zuora') -- CHANGE VIEW NAME
+      and t.table_name in (select tbl_name from a ) -- CHANGE VIEW NAME
 order by c.ordinal_position asc
 ;
