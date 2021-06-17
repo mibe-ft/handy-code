527bc9524c236592772d3fbce2cc9f1d230885e1
Maka Ibe <maka.ibe@ft.com>
2021-06-17 12:43:31 +0100

    Clean up query

--- jira-tickets/BI-1170/create_daily_step_up_table.sql
+++ jira-tickets/BI-1170/create_daily_step_up_table.sql
@@ -108,20 +108,65 @@ WITH step_01 AS (
 			   WHEN is_cancelled OR has_cancel_request THEN 'cancelled or cancel request'
 			   END AS renewal_step_up
 	FROM step_01
+), step_03 AS (
+
+	SELECT
+		d.*
+		, CASE WHEN s.email_id IS NULL THEN -9999 ELSE s.email_id END AS email_id
+		, CASE WHEN s.email_id = 1 THEN 'Digital - Step Up In Price'
+			   WHEN s.email_id = 2 THEN 'Digital - Renewal - No Price Change'
+			   WHEN s.email_id = 3 THEN 'Print - Step Up In Price'
+			   WHEN s.email_id = 4 THEN 'Print - Renewal - No Price Change'
+			   ELSE 'Not Applicable'
+			   END AS email_type_description
+		, s.send_comms
+	FROM step_02 AS d
+	LEFT JOIN #step_up_comms_logic AS s ON d.print_or_digital = s.print_or_digital
+										AND d.product_term_adjusted = s.term
+										AND d.renewal_step_up = s.renewal_step_up
+									 	AND d.is_control = s.is_control
 )
 
 SELECT
-	d.*
-	, CASE WHEN s.email_id IS NULL THEN -9999 ELSE s.email_id END AS email_id
-	, CASE WHEN s.email_id = 1 THEN 'Digital - Step Up In Price'
-		   WHEN s.email_id = 2 THEN 'Digital - Renewal - No Price Change'
-		   WHEN s.email_id = 3 THEN 'Print - Step Up In Price'
-		   WHEN s.email_id = 4 THEN 'Print - Renewal - No Price Change'
-		   END AS email_type_description
-	, s.send_comms
-FROM step_02 AS d
-LEFT JOIN #step_up_comms_logic AS s ON d.print_or_digital = s.print_or_digital
-									AND d.product_term_adjusted = s.term
-									AND d.renewal_step_up = s.renewal_step_up
-									AND d.is_control = s.is_control
+	  ft_user_id
+	, arrangement_id
+	, date_
+	, print_or_digital
+	, current_price
+	, current_offer
+	, current_offer_id
+	, region
+	, currency_code
+	, product_name
+	, product_name_adjusted
+	, product_term_adjusted
+	, step_up_price
+	, step_up_offer_id
+	, step_up_percent_discount
+	, CASE WHEN is_standard_plus = 1 THEN TRUE
+		   WHEN is_standard_plus = 0 THEN FALSE
+		   ELSE NULL END AS is_standard_plus
+	, CASE WHEN is_cancelled = 1 THEN TRUE
+		   WHEN is_cancelled = 0 THEN FALSE
+		   ELSE NULL END AS is_cancelled
+	, CASE WHEN has_cancel_request = 1 THEN TRUE
+		   WHEN has_cancel_request = 0 THEN FALSE
+		   ELSE NULL END AS has_cancel_request
+	, CASE WHEN is_renewal = 1 THEN TRUE
+		   WHEN is_renewal = 0 THEN FALSE
+		   ELSE NULL END AS is_renewal
+	, CASE WHEN is_eligible_for_step_up = 1 THEN TRUE
+		   WHEN is_eligible_for_step_up = 0 THEN FALSE
+		   ELSE NULL END AS is_eligible_for_step_up
+	, days_until_anniversary
+	, CASE WHEN is_control = 1 THEN TRUE
+		   WHEN is_control = 0 THEN FALSE
+		   ELSE NULL END AS is_control
+	, renewal_step_up
+	, email_id
+	, email_type_description
+	, CASE WHEN send_comms = 1 THEN TRUE
+		   WHEN send_comms = 0 THEN FALSE
+		   ELSE NULL END AS send_comms
+FROM step_03
 ;
