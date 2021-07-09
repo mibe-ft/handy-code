f767a6fcb9922eb8525d0d9e94952aca7523f2ef
Maka Ibe <maka.ibe@ft.com>
2021-07-09 12:29:31 +0100

    Add queries for comparing titles

--- jira-tickets/BI-1249/adhoc_queries.sql
+++ jira-tickets/BI-1249/adhoc_queries.sql
@@ -8,3 +8,29 @@ select count (distinct lower(job_title)) from ftlighthousedb.dim_user --96239 ;
 select distinct industry from ftlighthousedb.dim_user -- 32;
 
 select distinct position from ftlighthousedb.dim_user --23;
+
+-- join job titles
+with b2b_job_titles as (
+select lower(job_title) job_title , lower(job_type) job_type from biteam.b2b_job_titles),
+lighthouse_job_titles as (
+select lower(job_title) job_title, position, industry, count(distinct ft_user_id) users from ftlighthousedb.dim_user
+where job_title is not null
+group by 1,2,3
+order by 4 desc
+), jobs_without_nulls as (
+select a.job_title
+, a.users
+, b.job_title as job_title_a
+, b.job_type
+from lighthouse_job_titles a
+left join b2b_job_titles b on a.job_title = b.job_title
+where b.job_title is not null
+order by a.users desc
+)
+select
+(select sum(users) from jobs_without_nulls) as joined_without_nulls_sum,
+(select sum(users) from lighthouse_job_titles) as original
+--select * from jobs_without_nulls
+--select sum(users)
+
+;
