7b9a270f347fda209036964a4f8e57207010500c
Maka Ibe <maka.ibe@ft.com>
2021-07-09 09:47:10 +0100

    Add queries

--- jira-tickets/BI-1249/adhoc_queries.sql
+++ jira-tickets/BI-1249/adhoc_queries.sql
@@ -1,8 +1,10 @@
-select job_title, position, industry, count(distinct ft_user_id) users from ftlighthousedb.dim_user
+select lower(job_title) job_title, position, industry, count(distinct ft_user_id) users from ftlighthousedb.dim_user
 where job_title is not null
 group by 1,2,3
-order by 4 desc;
-
-select count(distinct lower(job_title)) from ftlighthousedb.dim_user
-where job_title is not null
+order by 4 desc
 ;
+
+select count (distinct lower(job_title)) from ftlighthousedb.dim_user --96239 ;
+select distinct industry from ftlighthousedb.dim_user -- 32;
+
+select distinct position from ftlighthousedb.dim_user --23;
