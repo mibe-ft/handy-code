9131dcc93d781184052606f1030f5812744688ea
Maka Ibe <maka.ibe@ft.com>
2021-08-05 12:38:20 +0100

    Add control group to queries

--- jira-tickets/BI-1170/create_biteam_step_up_tables.sql
+++ jira-tickets/BI-1170/create_biteam_step_up_tables.sql
@@ -59,3 +59,39 @@ CREATE TABLE biteam.stg_step_up_b2c_zuora_daily (
 	, send_comms BOOLEAN
 
 );
+
+DROP TABLE IF EXISTS biteam.step_up_b2c_zuora_daily_control_group;
+CREATE TABLE biteam.step_up_b2c_zuora_daily_control_group (
+      ft_user_id CHAR(36) NOT NULL
+    , arrangement_id INTEGER
+    , date_ TIMESTAMP
+    , step_up_month VARCHAR
+    , print_or_digital VARCHAR
+    , current_price NUMERIC(20,4)
+    , current_offer VARCHAR
+    , current_offer_id VARCHAR
+    , marketing_region VARCHAR
+    , currency_code CHAR(3)
+    , product_name VARCHAR
+    , product_name_adjusted VARCHAR
+    , product_term_adjusted VARCHAR
+    , step_up_price NUMERIC(20,4)
+    , step_up_offer_id VARCHAR
+    , step_up_percent_discount NUMERIC(10,2)
+    , is_standard_plus BOOLEAN
+    , is_cancelled BOOLEAN
+    , has_cancel_request BOOLEAN
+    , is_renewal BOOLEAN
+    , is_eligible_for_step_up BOOLEAN
+    , days_until_anniversary SMALLINT
+    , email_address VARCHAR
+    , subs_product VARCHAR
+    , product VARCHAR
+    , title VARCHAR
+    , firstname VARCHAR
+    , surname VARCHAR
+    , current_country_name VARCHAR
+    , campaign_region VARCHAR
+    , industry VARCHAR
+    , subs_term VARCHAR
+)
